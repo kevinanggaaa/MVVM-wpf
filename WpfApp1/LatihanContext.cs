8556dda9551124cde3accf05974fbb2ac4ee7c86
kevinanggaaa <60419316+kevinanggaaa@users.noreply.github.com>
2021-04-12 20:57:06 +0700

    Add project files.

---
+++ WpfApp1/LatihanContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace WpfApp1
+{
+    class LatihanContext : DbContext
+    {   
+        public DbSet<ItemPenjualan> DaftarItemPenjualan {
+            get; set;
+        }
+    }
+}
